17b43af82d019cd3ceac9ad1b64ede73ef1ec0d7
unknown <akash.agrawal445@gmail.com>
2025-08-15 10:57:57 +0530

    Inception class finish

---
+++ class1/App.js
@@ -0,0 +1,31 @@
+/**
+ * <div id="parent">
+ *  <div id="child1">
+ *    <h1> I'm in h1 tag </h1>
+ *    <h2> I'm in h2 tag </h2>
+ *  </div>
+ *  <div id="child2">
+ *    <h1> I'm in h1 tag </h1>
+ *    <h2> I'm in h2 tag </h2>
+ *  </div>
+ * </div>
+ *
+ */
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "check" },
+//   "First React Program"
+// );
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm in h1 tag"),
+    React.createElement("h2", {}, "I'm in h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm in h1 tag"),
+    React.createElement("h2", {}, "I'm in h2 tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
