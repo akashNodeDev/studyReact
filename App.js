c6aa5e2fe2f40d56c053997eb94ea99b8541a2a5
unknown <akash.agrawal445@gmail.com>
2025-08-31 16:48:24 +0530

    code example for jsx

--- App.js
+++ App.js
@@ -1,33 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/**
- * <div id="parent">
- *  <div id="child1">
- *    <h1> I'm in h1 tag </h1>
- *    <h2> I'm in h2 tag </h2>
- *  </div>
- *  <div id="child2">
- *    <h1> I'm in h1 tag </h1>
- *    <h2> I'm in h2 tag </h2>
- *  </div>
- * </div>
- *
- */
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "check" },
-//   "First React Program"
-// );
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    React.createElement("h1", { key: "child1-h1" }, "I'm in h1 tag"),
-    React.createElement("h2", { key: "child1-h2" }, "I'm in h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "child2-h1" }, "I'm in h1 tag"),
-    React.createElement("h2", { key: "child2-h2" }, "I'm in h2 tag"),
-  ]),
-]);
+//JSX (transpiled before it reaches to the JS) - Parcel - Babel
+
+//JSX => Babel Transpiles it to React.createElement => React Element JS Object => HTML Element (render)
+const jsxHeading = <h1 className="heading">Hello My First JSX Program</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
