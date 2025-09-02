de4d99cb6f933c936bc6fd9954a0a82ac8a1413b
unknown <akash.agrawal445@gmail.com>
2025-09-02 08:55:51 +0530

    Concept of the functional component and the element calling

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX (transpiled before it reaches to the JS) - Parcel - Babel
+// React Element
+const titleElement = (
+  <div className="heading" id="heading">
+    React Element Example
+  </div>
+);
 
-//JSX => Babel Transpiles it to React.createElement => React Element JS Object => HTML Element (render)
-const jsxHeading = <h1 className="heading">Hello My First JSX Program</h1>;
+const newTitle = <span>Hello</span>;
+
+// React Functional Component
+const Title = () => (
+  <h1 className="heading" id="heading">
+    React Functional Component Example
+  </h1>
+);
+
+// React Functional Component
+const num = 100;
+const HeadingComponent = () => (
+  <div id="container">
+    {/* We can call the functional component inside another functional component in the below three ways*/}
+    {/**Also we can write any pce of the javascript code inside the functional component in the curly braces */}
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1>Ths is an exmple of Functional Component</h1>
+    {/** Example of calling an element inside the functional component */}
+    {titleElement}
+    {/** Example of calling a JS variable inside the functional component */}
+    <b>{num}</b>
+    <b>{newTitle}</b>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
