9b777eaa83c62fb45caca1b544c4fe7773f73644
unknown <akash.agrawal445@gmail.com>
2025-09-19 09:04:07 +0530

    static card creation for class 4 part1

--- App.js
+++ App.js
@@ -1,39 +1,106 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const titleElement = (
-  <div className="heading" id="heading">
-    React Element Example
-  </div>
-);
+/**
+ * Swiggy Like Website Structure
+ /**
+  * Header
+  *   - Logo
+  *   - Nav Items
+  *   - Cart
+  * 
+  * Body
+  *   - Search
+  *   - Resturant Container
+  *       - Resturant Card
+  *           - Image
+  *           - Res Name,Cusines,Star Rating,Delivery Time etc.
+  * 
+  * Footer
+  *   - Links
+  *   - Address
+  *   - Contact
+  * 
+  */
 
-const newTitle = <span>Hello</span>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-vector/20250512/ourmid/pngtree-spicy-eat-resturent-logo-png-image_16234139.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About US</li>
+          <li>Contact US</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// React Functional Component
-const Title = () => (
-  <h1 className="heading" id="heading">
-    React Functional Component Example
-  </h1>
-);
+const styleCard = {
+  backgroundColor: "#F0F0F0",
+};
 
-// React Functional Component
-const num = 100;
-const HeadingComponent = () => (
-  <div id="container">
-    {/* We can call the functional component inside another functional component in the below three ways*/}
-    {/**Also we can write any pce of the javascript code inside the functional component in the curly braces */}
-    <Title />
-    <Title></Title>
-    {Title()}
-    <h1>Ths is an exmple of Functional Component</h1>
-    {/** Example of calling an element inside the functional component */}
-    {titleElement}
-    {/** Example of calling a JS variable inside the functional component */}
-    <b>{num}</b>
-    <b>{newTitle}</b>
-  </div>
-);
+const ResturantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      {/* We can also write like  style={{backgroundColor: "#F0F0F0"}} Here we give the css in the form of the JS object */}
+      <img
+        className="res-logo"
+        alt="card-image"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/2/27/1489cae0-db9d-4050-aa9d-a058decede06_994876.jpg"></img>
+      <h3>Cafe Amudham</h3>
+      <h4> South Indian, Home Food </h4>
+      <h4> 4.5 Stars </h4>
+      <h4>50-60 mins </h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
