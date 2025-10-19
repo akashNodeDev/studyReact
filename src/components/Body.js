77dbcd4d42e4e05d68f7557715f4eb6d4f4323b3
unknown <akash.agrawal445@gmail.com>
2025-10-19 20:12:17 +0530

    added the useState hook example using the filtering the resturant list

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,29 @@
 import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  // Local State Variable
+  const [listOfResturants, setListOfResturant] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            //console.log("Res List Changed");
+            const filteredList = listOfResturants.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setListOfResturant(filteredList);
+            console.log("Res List filtered", filteredList);
+          }}>
+          Top Rated Resturants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((resturant) => (
+        {listOfResturants.map((resturant) => (
           <ResturantCard key={resturant.info.id} resData={resturant} />
         ))}
       </div>
