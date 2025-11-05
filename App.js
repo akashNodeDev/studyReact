94e05d5419b301709da046d3ccdd6d302cc97280
unknown <akash.agrawal445@gmail.com>
2025-11-05 13:29:56 +0530

    code added for the routing basics

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import Error from "./src/components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 /**
  * Swiggy Like Website Structure
@@ -34,5 +38,23 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+//root.render(<AppLayout />);
+
+root.render(<RouterProvider router={appRouter} />);
